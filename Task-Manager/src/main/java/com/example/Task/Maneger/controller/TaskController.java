f2889b0895241e2175911ece2ca16ca68ed49e50
Yussuf Ahmed <mtawolfroleplay@gmail.com>
2024-10-06 15:37:34 +0300

    .

--- Task-Manager/src/main/java/com/example/Task/Maneger/controller/TaskController.java
+++ Task-Manager/src/main/java/com/example/Task/Maneger/controller/TaskController.java
@@ -23,47 +23,35 @@ public class TaskController {
     EmailService emailService;
     @Autowired
     TaskService taskService;
-
     @GetMapping("/tasks")
     public String home(Model model, Principal principal) {
        taskService.showTasks(principal, model);
         return "tasks";
     }
-
     @PostMapping("/tasks/create")
     public String createTask(@ModelAttribute TaskModel task, Principal principal) {
        taskService.createTask(task, principal);
        return TASKS_REDIRECT;
     }
-
     @GetMapping("/tasks/create")
     public String getCreatTask(Model model) {
         model.addAttribute("taskModel", new TaskModel());
         return "createtask";
     }
-
     @PostMapping("/tasks/delete/{id}")
     public String deleteTask(@PathVariable long id) {
         tasksRepository.deleteById(id);
         return TASKS_REDIRECT;
     }
-
     @PostMapping("/tasks/edit/{id}")
     public String editTask(@PathVariable long id, @ModelAttribute("task") TaskModel updatedTaskModel) {
        taskService.edittask(id,updatedTaskModel);
-
        return TASKS_REDIRECT;
     }
-
         @GetMapping("/tasks/edit/{id}")
-
         public String getEditTask ( @PathVariable long id, Model model, Principal principal){
             taskService.getedittask(id, model, principal);
             return "edittask";
     }
-
-
-
-
 }
 
